c3514a55ef1e47358e78219003eb16942ab3bc10
Christopher Kohlhaas <christopher.kohlhaas@gmail.com>
2015-07-16 17:10:24 +0200

    plugin in myrapla succeeded

---
+++ src/main/java/org/rapla/gwt/client/myplugin/MyRaplaGreeter.java
@@ -0,0 +1,13 @@
+package org.rapla.gwt.client.myplugin;
+
+import org.rapla.gwt.client.Greeter;
+
+public class MyRaplaGreeter implements Greeter
+{
+
+    public String greet()
+    {
+        return "Hello Plugin to Rapla ";
+    }
+
+}
