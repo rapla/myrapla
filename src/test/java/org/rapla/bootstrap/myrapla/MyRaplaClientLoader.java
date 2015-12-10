dbbc22140c032eb84465d018bcdb2b7cec23b2f4
Martin Geuer <martin.geuer@erdkante.de>
2015-12-10 10:12:51 +0100

    myrapla starts in eclipse

---
+++ src/test/java/org/rapla/bootstrap/myrapla/MyRaplaClientLoader.java
@@ -0,0 +1,14 @@
+package org.rapla.bootstrap.myrapla;
+
+import java.io.IOException;
+
+import org.rapla.bootstrap.RaplaClientLoader;
+
+public class MyRaplaClientLoader
+{
+
+    public static void main(String[] args) throws IOException
+    {
+        RaplaClientLoader.main(args);
+    }
+}
---
+++ src/test/java/org/rapla/bootstrap/myrapla/MyRaplaServerLoader.java
@@ -0,0 +1,12 @@
+package org.rapla.bootstrap.myrapla;
+
+import org.rapla.bootstrap.RaplaServerLoader;
+
+public class MyRaplaServerLoader
+{
+    public static void main(String[] args) throws Exception
+    {
+        RaplaServerLoader.main(args);
+    }
+
+}
